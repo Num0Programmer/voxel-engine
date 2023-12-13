7d276638a3ddb0232805b26968ef656eec4850a7
Num0Programmer <asajh4@gmail.com>
2023-12-12 21:45:42 -0700

    Implemented creation of a logical device for interacting with first found physical device.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use vulkano::{
     instance::{Instance, InstanceCreateInfo, InstanceExtensions},
-    device::physical::PhysicalDevice,
+    device::{Device, DeviceCreateInfo, DeviceExtensions, Features, QueueCreateInfo},
     VulkanLibrary
 };
 
@@ -25,18 +25,38 @@ pub fn main()
             ..Default::default()
         }
     ).unwrap_or_else(|err| panic!("Could not create Vulkan Instance: {:?}", err));
-
-    // show user their vk library, extensions, and instance
-    println!("Loaded library: {:?}\n", library);
-    println!("Extensions: {:?}\n", extensions);
-    println!("Instance: {:?}\n", instance);
-    // show user available devices for VK
-    println!("Physical devices found:");
-    for physical_device in instance
+    let physical_device = instance
         .enumerate_physical_devices()
         .unwrap_or_else(|err| panic!("Could not get physical device: {:?}", err))
-    {
-        println!("Available device: {:?}", physical_device.properties().device_name);
-        println!("                  {:?}", physical_device.properties().device_id);
-    }
+        .next().expect("No physical device!");
+
+
+    // choose first physical device found
+    let device = {
+        let dev_features = Features::empty();
+        let dev_extensions = DeviceExtensions::empty();
+
+        match Device::new(
+            physical_device,
+            DeviceCreateInfo
+            {
+                queue_create_infos: vec![QueueCreateInfo
+                {
+                    queue_family_index: 0,
+                    ..Default::default()
+                }],
+                enabled_extensions: dev_extensions,
+                enabled_features: dev_features,
+                ..Default::default()
+            },
+        ) {
+            Ok(d) => d,
+            Err(err) => panic!("Couldn't build device: {:?}", err)
+        }
+    };
+
+    // show user the extensions
+    println!("Extensions: {:?}\n", extensions);
+    // show user the created device
+    println!("Logical device: {:?}", device.0);
 }
