a2ecbd8fa4f76952d539f5f787483c0a1fa6e249
Num0Programmer <asajh4@gmail.com>
2023-12-17 12:58:58 -0700

    Implemented creation of a logical device, and choosing of a queue family. More robust choosing process will be implemented later once things are drawn to the screen.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use winit::{event_loop::EventLoop, window::WindowBuilder};
 use std::sync::Arc;
 use vulkano::{
+    device::{Device, DeviceCreateInfo, DeviceExtensions, QueueCreateInfo},
     instance::{Instance, InstanceCreateInfo, InstanceCreateFlags},
     swapchain::Surface,
     VulkanLibrary
@@ -9,9 +10,12 @@ use vulkano::{
 
 pub fn main()
 {
-    // define loop for winit events
     let event_loop = EventLoop::new();
-    // setup vulkan instance and choose a physical device
+    let window = Arc::new(WindowBuilder::new()
+        .build(&event_loop)
+        .unwrap_or_else(|err| panic!("Could not build window: {:?}", err))
+    );
+
     let library = VulkanLibrary::new()
         .unwrap_or_else(|err| panic!("Could not load Vulkan Library: {:?}", err));
     let required_extensions = Surface::required_extensions(&event_loop);
@@ -24,16 +28,40 @@ pub fn main()
             ..Default::default()
         })
         .unwrap_or_else(|err| panic!("Could not create instance: {:?}", err));
-    let physical_device = instance
-        .enumerate_physical_devices()
-        .unwrap_or_else(|err| panic!("Could not enumerate physical devices: {:?}", err))
-        .next().expect("No physical device!");
-
-    // setup window and surface onto which to draw
-    let window = Arc::new(WindowBuilder::new()
-        .build(&event_loop)
-        .unwrap_or_else(|err| panic!("Could not build window: {:?}", err)));
+    
     let surface = Surface::from_window(instance.clone(), window.clone()).unwrap();
 
-    println!("Created surface: {:?}", surface);
+    let dev_extensions = DeviceExtensions
+    {
+        khr_swapchain: true,
+        ..DeviceExtensions::empty()
+    };
+    let physical_device = instance.enumerate_physical_devices()
+        .unwrap_or_else(|err| panic!("Could not enumerate physical devices: {:?}", err))
+        .next().expect("No physical device!");
+    let queue_family_index = 0;  // will be set dynamically as I learn the API
+
+    println!("Physical device:\n{} (type: {:?})\n",
+        physical_device.properties().device_name,
+        physical_device.properties().device_type
+    );
+
+    let (device, mut queues) = Device::new(
+        physical_device,
+        DeviceCreateInfo
+        {
+            enabled_extensions: dev_extensions,
+            queue_create_infos: vec![QueueCreateInfo
+            {
+                queue_family_index,
+                ..Default::default()
+            }],
+            ..Default::default()
+        }
+    ).unwrap();
+
+    let queue = queues.next().unwrap();
+
+    println!("Logical device: {:?}\n", device);
+    println!("Queue family: {:?}\n", queue);
 }
