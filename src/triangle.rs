cdb4d13cd750b6e567709f7ef0daae21ff2fe500
Num0Programmer <asajh4@gmail.com>
2023-12-27 15:15:06 -0700

    Added logic in vertex and fragment shaders so as to reflect the triangle in the 'Drawing a triangle' section of the Vulkan tutorial produces by the Khrono Group. This should also help expound on the understanding that 'in' and 'out' keywords are used by the shader language to identify areas in memory (i.e. the variable) which will be passed to the next shader in the pipeline.

--- src/triangle.rs
+++ src/triangle.rs
@@ -164,15 +164,15 @@ pub fn main()
     let vertices = [
         Vertex
         {
-            position: [-0.5, -0.25]
+            position: [0.0, -0.5]
         },
         Vertex
         {
-            position: [0.0, 0.5]
+            position: [0.5, 0.5]
         },
         Vertex
         {
-            position: [0.25, -0.1]
+            position: [-0.5, 0.5]
         }
     ];
     let vertex_buffer = Buffer::from_iter(
@@ -199,11 +199,19 @@ pub fn main()
             src: r"
                 #version 450
 
+                vec3 colors[3] = vec3[](
+                    vec3(1.0, 0.0, 0.0),
+                    vec3(0.0, 1.0, 0.0),
+                    vec3(0.0, 0.0, 1.0)
+                );
+
                 layout (location = 0) in vec2 position;
+                layout (location = 0) out vec3 f_color;
 
                 void main()
                 {
                     gl_Position = vec4(position, 0.0, 1.0);
+                    f_color = colors[gl_VertexIndex];
                 }
             "
         }
@@ -216,11 +224,12 @@ pub fn main()
             src: r"
                 #version 450
 
-                layout(location = 0) out vec4 f_color;
+                layout (location = 0) in vec3 f_color;
+                layout (location = 0) out vec4 o_color;
 
                 void main()
                 {
-                    f_color = vec4(1.0, 0.0, 0.0, 1.0);
+                    o_color = vec4(f_color, 1.0);
                 }
             "
         }
